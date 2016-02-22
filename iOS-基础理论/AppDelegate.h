d2e4c967fcd86ef73579d75263dbc89c710434e7
王潇 <23601145@qq.com>
2016-02-22 20:25:17 +0800

    Initial Commit

---
+++ iOS-基础理论/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS-基础理论
+//
+//  Created by 王潇 on 16/2/22.
+//  Copyright © 2016年 王潇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS-基础理论/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOS-基础理论
+//
+//  Created by 王潇 on 16/2/22.
+//  Copyright © 2016年 王潇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
